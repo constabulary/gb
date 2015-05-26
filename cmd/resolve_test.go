ec5b7c27d4493313e92aeb777606ace7604c1860
Dave Cheney <dave@cheney.net>
2015-05-26 22:47:28 +1000

    Don't mask build.NoGoErr
    Fixes #131

--- cmd/resolve_test.go
+++ cmd/resolve_test.go
@@ -18,6 +18,7 @@ func TestResolvePackages(t *testing.T) {
 	}{
 		{paths: []string{"a"}},
 		{paths: []string{"."}, err: fmt.Errorf("%q is not a package", root)},
+		{paths: []string{"h"}, err: fmt.Errorf("failed to resolve import path %q: no buildable Go source files in %s", "h", filepath.Join(root, "blank"))},
 	}
 
 	for _, tt := range tests {
