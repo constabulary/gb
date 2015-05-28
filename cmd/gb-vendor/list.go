db8e9bda2ac0a802abec9f91aad9152d4367d32b
Dave Cheney <dave@cheney.net>
2015-05-28 21:32:42 +1000

    cmd/gb-vendor: add gb vendor list subcommand

---
+++ cmd/gb-vendor/list.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"html/template"
+	"os"
+
+	"github.com/constabulary/gb"
+	"github.com/constabulary/gb/cmd"
+	"github.com/constabulary/gb/cmd/gb-vendor/vendor"
+)
+
+func init() {
+	registerCommand("list", List)
+}
+
+var format string
+
+var List = &cmd.Command{
+	ShortDesc: "lists the packages named by the import paths, one per line.",
+	Run: func(ctx *gb.Context, args []string) error {
+		m, err := vendor.ReadManifest(manifestFile(ctx))
+		if err != nil {
+			return fmt.Errorf("could not load manifest: %v", err)
+		}
+		tmpl, err := template.New("list").Parse(format)
+		if err != nil {
+			return fmt.Errorf("unable to parse template %q: %v", format, err)
+		}
+
+		for _, dep := range m.Dependencies {
+			if err := tmpl.Execute(os.Stdout, dep); err != nil {
+				return fmt.Errorf("unable to execute template: %v", err)
+			}
+			fmt.Fprintln(os.Stdout)
+		}
+		return nil
+	},
+	AddFlags: func(fs *flag.FlagSet) {
+		fs.StringVar(&format, "f", "{{.Importpath}}\t{{.Repository}}{{.Path}}\t{{.Branch}}\t{{.Revision}}", "format template")
+	},
+}
