01545363b274b1ee23166aadc8613f9a89b871be
Dave Cheney <dave@cheney.net>
2015-05-29 21:30:42 +1000

    cmd/gb: add gb doc
    gb doc is a simple wrapper around go doc / godoc. Invocation is the same
    
        gb doc $PACKAGE
        gb doc $PACKAGE $SYMBOL
    
    Without arguments gb doc behaves as if you had passed `gb doc .` which
    expands to the current working directory.

---
+++ cmd/gb/doc.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/constabulary/gb"
+	"github.com/constabulary/gb/cmd"
+)
+
+func init() {
+	registerCommand("doc", &cmd.Command{
+		ShortDesc: "show documentation for a package or symbol",
+		Run: func(ctx *gb.Context, args []string) error {
+			env := cmd.MergeEnv(os.Environ(), map[string]string{
+				"GOPATH": fmt.Sprintf("%s:%s", ctx.Projectdir(), filepath.Join(ctx.Projectdir(), "vendor")),
+			})
+			if len(args) == 0 {
+				args = append(args, ".")
+			}
+			args = append([]string{filepath.Join(ctx.GOROOT, "bin", "godoc")}, args...)
+
+			cmd := exec.Cmd{
+				Path: args[0],
+				Args: args,
+				Env:  env,
+
+				Stdin:  os.Stdin,
+				Stdout: os.Stdout,
+				Stderr: os.Stderr,
+			}
+			return cmd.Run()
+		},
+		ParseArgs: func(_ *gb.Context, _ string, args []string) []string { return args },
+	})
+}
