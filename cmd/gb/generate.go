ca4f06c57f0a8bfdee5231f42d1df82ceb0d3751
Dave Cheney <dave@cheney.net>
2015-05-29 17:11:51 +1000

    cmd/gb: added generate subcommand
    Fixes #148
    
    Simple hack to call `go generate`, we can do something more complicated later.

---
+++ cmd/gb/generate.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/constabulary/gb"
+	"github.com/constabulary/gb/cmd"
+)
+
+func init() {
+	registerCommand("generate", &cmd.Command{
+		ShortDesc: "generate Go files by processing source",
+		Run: func(ctx *gb.Context, args []string) error {
+			env := cmd.MergeEnv(os.Environ(), map[string]string{
+				"GOPATH": fmt.Sprintf("%s:%s", ctx.Projectdir(), filepath.Join(ctx.Projectdir(), "vendor")),
+			})
+
+			args = []string{filepath.Join(ctx.GOROOT, "bin", "go"), "generate"}
+
+			cmd := exec.Cmd{
+				Path: args[0],
+				Args: args,
+				Env:  env,
+
+				Stdin:  os.Stdin,
+				Stdout: os.Stdout,
+				Stderr: os.Stderr,
+			}
+
+			return cmd.Run()
+		},
+	})
+}
