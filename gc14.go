3b99af6dde5a644645a2435f47316304940d53eb
Maciej Pasternacki <maciej@3ofcoins.net>
2015-05-29 16:34:05 +0200

    Use CC detection to use appropriate extld flag (fixes cgo links on FreeBSD)

--- gc14.go
+++ gc14.go
@@ -77,7 +77,7 @@ func (t *gcToolchain) Ld(pkg *Package, searchpaths, ldflags []string, outfile, a
 	for _, d := range searchpaths {
 		args = append(args, "-L", d)
 	}
-	args = append(args, "-extld=gcc") // TODO(dfc) go 1.5+, "-buildmode=exe")
+	args = append(args, "-extld="+gcc()) // TODO(dfc) go 1.5+, "-buildmode=exe")
 	args = append(args, afile)
 	if err := mkdir(filepath.Dir(outfile)); err != nil {
 		return fmt.Errorf("gc:ld: %v", err)
