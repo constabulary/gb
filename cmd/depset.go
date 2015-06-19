75dcc36759afb2e68ee02a648287ff5fe8fd8e85
Dave Cheney <dave@cheney.net>
2015-06-19 21:04:50 +1000

    cmd/gb: add experimental depset command
    depset is an experimental command to explore analysing dependency sets.
    It produces output like this, http://paste.ubuntu.com/11739677/
    
    At the moment it has worse than quadratic behaviour, making it unsuitable for anything serious.
    
    The goal of depset is to develop a set of functions in the `cmd` package that can be used by tools like `gb-vendor` to analyse dependencies as a set rather than as individual imports.
    Dependency sets are groups of packages that are related by being children of a single source root. A canonical example of a source root is $GOPATH/src, another is $PROJECT/src. However we can be smarter and create sets for each vendored import tree, which in turn allows us to determine which dependencies are no longer referenced with more accuracy than the current gb vendor purge command.
    
    Another function of dep sets is to determine missing packages, the output of which could be fed back into gb vendor fetch to drive recursive depdendency fetching.

---
+++ cmd/depset.go
@@ -0,0 +1,144 @@
+package cmd
+
+import (
+	"fmt"
+	"go/build"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func keys(m map[string]bool) []string {
+	var v []string
+	for k := range m {
+		v = append(v, k)
+	}
+	return v
+}
+
+// Pkg describes a Go package.
+type Pkg struct {
+	*Depset
+	*build.Package
+}
+
+// Depset describes a set of related Go packages.
+type Depset struct {
+	Root   string
+	Prefix string
+	Pkgs   map[string]*Pkg
+}
+
+// LoadPaths returns a map of paths to Depsets.
+func LoadPaths(paths ...struct{ Root, Prefix string }) (map[string]*Depset, error) {
+	m := make(map[string]*Depset)
+	for _, p := range paths {
+		set, err := LoadTree(p.Root, p.Prefix)
+		if err != nil {
+			return nil, err
+		}
+		m[set.Root] = set
+	}
+	return m, nil
+}
+
+// LoadTree parses a tree of source files into a map of *pkgs.
+func LoadTree(root string, prefix string) (*Depset, error) {
+	d := Depset{
+		Root:   root,
+		Prefix: prefix,
+		Pkgs:   make(map[string]*Pkg),
+	}
+	fn := func(dir string, fi os.FileInfo) error {
+		importpath := filepath.Join(prefix, dir[len(root)+1:])
+
+		// if we're at the root of a tree, skip it
+		if importpath == "" {
+			return nil
+		}
+
+		p, err := loadPackage(&d, dir, importpath)
+		if err != nil {
+			if _, ok := err.(*build.NoGoError); ok {
+				return nil
+			}
+			return fmt.Errorf("loadPackage(%q, %q): %v", dir, importpath, err)
+		}
+		p.ImportPath = importpath
+		if p != nil {
+			d.Pkgs[p.ImportPath] = p
+		}
+		return nil
+	}
+
+	// handle root of the tree
+	fi, err := os.Stat(root)
+	if err != nil {
+		return nil, err
+	}
+	if err := fn(root+"/", fi); err != nil {
+		return nil, err
+	}
+
+	// walk sub directories
+	err = eachDir(root, fn)
+	return &d, err
+}
+
+func loadPackage(d *Depset, dir, importpath string) (*Pkg, error) {
+	p := Pkg{
+		Depset: d,
+	}
+	var err error
+
+	// expolit local import logic
+	p.Package, err = build.ImportDir(dir, build.ImportComment)
+	return &p, err
+}
+
+func eachFile(dir string, fn func(string, os.FileInfo) error) error {
+	f, err := os.Open(dir)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	files, err := f.Readdir(-1)
+	if err != nil {
+		return err
+	}
+	for _, fi := range files {
+		if fi.IsDir() {
+			continue
+		}
+		path := filepath.Join(dir, fi.Name())
+		if err := fn(path, fi); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func eachDir(dir string, fn func(string, os.FileInfo) error) error {
+	f, err := os.Open(dir)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	files, err := f.Readdir(-1)
+	for _, fi := range files {
+		if !fi.IsDir() {
+			continue
+		}
+		if strings.HasPrefix(fi.Name(), "_") || strings.HasPrefix(fi.Name(), ".") || fi.Name() == "testdata" {
+			continue
+		}
+		path := filepath.Join(dir, fi.Name())
+		if err := fn(path, fi); err != nil {
+			return err
+		}
+		if err := eachDir(path, fn); err != nil {
+			return err
+		}
+	}
+	return nil
+}
