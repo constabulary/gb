6a95f1c802d5eb4419799d7a497ddbeca81aedb2
Dave Cheney <dave@cheney.net>
2015-06-19 21:04:50 +1000

    cmd/gb: add experimental depset command
    depset is an experimental command to explore analysing dependency sets.
    It produces output like this, http://paste.ubuntu.com/11739677/
    
    At the moment it has worse than quadratic behaviour, making it unsuitable for anything serious.
    
    The goal of depset is to develop a set of functions in the `cmd` package that can be used by tools like `gb-vendor` to analyse dependencies as a set rather than as individual imports.
    Dependency sets are groups of packages that are related by being children of a single source root. A canonical example of a source root is $GOPATH/src, another is $PROJECT/src. However we can be smarter and create sets for each vendored import tree, which in turn allows us to determine which dependencies are no longer referenced with more accuracy than the current gb vendor purge command.
    
    Another function of dep sets is to determine missing packages, the output of which could be fed back into gb vendor fetch to drive recursive depdendency fetching.

---
+++ cmd/gb/stringset_test.go
@@ -0,0 +1,147 @@
+package main
+
+import "testing"
+import "reflect"
+
+func set(args ...string) map[string]bool {
+	r := make(map[string]bool)
+	for _, a := range args {
+		r[a] = true
+	}
+	return r
+}
+
+func TestUnion(t *testing.T) {
+	tests := []struct {
+		a, b map[string]bool
+		want map[string]bool
+	}{{
+		a: nil, b: nil,
+		want: set(),
+	}, {
+		a: nil, b: set("b"),
+		want: set("b"),
+	}, {
+		a: set("a"), b: nil,
+		want: set("a"),
+	}, {
+		a: set("a"), b: set("b"),
+		want: set("b", "a"),
+	}, {
+		a: set("c"), b: set("c"),
+		want: set("c"),
+	}}
+
+	for _, tt := range tests {
+		got := union(tt.a, tt.b)
+		if !reflect.DeepEqual(tt.want, got) {
+			t.Errorf("union(%v, %v) want: %v, got %v", tt.a, tt.b, tt.want, got)
+		}
+	}
+}
+
+func TestIntersection(t *testing.T) {
+	tests := []struct {
+		a, b map[string]bool
+		want map[string]bool
+	}{{
+		a: nil, b: nil,
+		want: set(),
+	}, {
+		a: nil, b: set("b"),
+		want: set(),
+	}, {
+		a: set("a"), b: nil,
+		want: set(),
+	}, {
+		a: set("a"), b: set("b"),
+		want: set(),
+	}, {
+		a: set("c"), b: set("c"),
+		want: set("c"),
+	}, {
+		a: set("a", "c"), b: set("b", "c"),
+		want: set("c"),
+	}}
+
+	for _, tt := range tests {
+		got := intersection(tt.a, tt.b)
+		if !reflect.DeepEqual(tt.want, got) {
+			t.Errorf("intersection(%v, %v) want: %v, got %v", tt.a, tt.b, tt.want, got)
+		}
+	}
+}
+
+func TestDifference(t *testing.T) {
+	tests := []struct {
+		a, b map[string]bool
+		want map[string]bool
+	}{{
+		a: nil, b: nil,
+		want: set(),
+	}, {
+		a: nil, b: set("b"),
+		want: set("b"),
+	}, {
+		a: set("a"), b: nil,
+		want: set("a"),
+	}, {
+		a: set("a"), b: set("b"),
+		want: set("a", "b"),
+	}, {
+		a: set("c"), b: set("c"),
+		want: set(),
+	}, {
+		a: set("a", "c"), b: set("b", "c"),
+		want: set("a", "b"),
+	}}
+
+	for _, tt := range tests {
+		got := difference(tt.a, tt.b)
+		if !reflect.DeepEqual(tt.want, got) {
+			t.Errorf("difference(%v, %v) want: %v, got %v", tt.a, tt.b, tt.want, got)
+		}
+	}
+}
+
+func TestContains(t *testing.T) {
+	tests := []struct {
+		a    map[string]bool
+		s    []string
+		want bool
+	}{{
+		a: nil, s: nil,
+		want: false,
+	}, {
+		a: set("a"), s: nil,
+		want: false,
+	}, {
+		a: set("a"), s: []string{"a"},
+		want: true,
+	}, {
+		a: set("a"), s: []string{"b"},
+		want: false,
+	}, {
+		a: set("a", "b"), s: []string{"b"},
+		want: true,
+	}, {
+		a: set("a"), s: []string{"a", "b"},
+		want: false,
+	}, {
+		a: set("a", "b", "c"), s: []string{"a", "b"},
+		want: true,
+	}, {
+		a: set("a", "b", "c"), s: []string{"x", "b"},
+		want: false,
+	}, {
+		a: set("a", "b", "c"), s: []string{"b", "c", "d"},
+		want: false,
+	}}
+
+	for _, tt := range tests {
+		got := contains(tt.a, tt.s...)
+		if !reflect.DeepEqual(tt.want, got) {
+			t.Errorf("contains(%v, %v) want: %v, got %v", tt.a, tt.s, tt.want, got)
+		}
+	}
+}
