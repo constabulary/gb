123a936ac14d033c641cd27f062bcc9f635b5861
Dave Cheney <dave@cheney.net>
2015-07-12 23:04:58 -0600

    cmd/gb: add gb info subcommand
    Fixes #267
    
    This PR adds a new subcommand, `info`. This returns some (hopefully) pertinent
    information about the project, and an error code to indicate if the path is part
    of a gb project or not.
    
    	zapf(~/devel/demo4) % gb info
    	GB_PROJECT_DIR="/Users/dfc/devel/demo4"
    	GB_SRC_PATH="/Users/dfc/devel/demo4/src:/Users/dfc/devel/demo4/vendor/src"
    	zapf(~/devel/demo4) % echo $?
    	0
    
    	zapf(~/devel/demo9) % gb info
    	FATAL could not locate project root: could not find project root in "/Users/dfc/devel/demo9" or its parents
    	zapf(~/devel/demo9) % echo $?                                                                                                                                            1

---
+++ cmd/gb/info.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	"github.com/constabulary/gb"
+	"github.com/constabulary/gb/cmd"
+)
+
+func init() {
+	registerCommand(&cmd.Command{
+		Name:      "info",
+		UsageLine: `info`,
+		Short:     "info returns information about this project",
+		Long: `info returns information about this project.
+
+info returns 0 if the project is well formed, and non zero otherwise.
+`,
+		Run: info,
+	})
+}
+
+func info(ctx *gb.Context, args []string) error {
+	fmt.Printf("GB_PROJECT_DIR=%q\n", ctx.Projectdir())
+	fmt.Printf("GB_SRC_PATH=%q\n", joinlist(ctx.Srcdirs()...))
+	return nil
+}
+
+// joinlist joins path elements using the os specific separator.
+// TODO(dfc) it probably gets this wrong on windows in some circumstances.
+func joinlist(paths ...string) string {
+	return strings.Join(paths, string(filepath.ListSeparator))
+}
