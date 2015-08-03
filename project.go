9d7daa78994be96768355172e95ab744078f1d9d
Dave Cheney <dave@cheney.net>
2015-08-03 20:30:46 +1000

    Remove old commentary for a program that is long dead.

--- project.go
+++ project.go
@@ -9,8 +9,6 @@ import (
 // starting at the project root, which we'll refer too as $PROJECT.
 //
 //     $PROJECT/                       - the project root
-//     $PROJECT/.gogo/                 - used internally by gogo and identifies
-//                                       the root of the project.
 //     $PROJECT/src/                   - base directory for the source of packages
 //     $PROJECT/bin/                   - base directory for the compiled binaries
 type Project struct {
