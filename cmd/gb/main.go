5a51b3b8b86c82fc8fd98158ad4f58ff5b0bc4e0
Dave Cheney <dave@cheney.net>
2015-08-09 14:26:06 +1000

    Squashed commit of the following:
    commit 295f57c1da99f34f58c3897efea1f2dfa3e09bcb
    Author: Dave Cheney <dave@cheney.net>
    Date:   Sun Aug 9 14:12:51 2015 +1000
    
        sigh
    
        aaaah, the magic flag
    
        aaaah, the magic flag
    
    commit b7e76ae879b1513d1fdc1320c4d7a8ab37f503fb
    Author: Dave Cheney <dave@cheney.net>
    Date:   Sun Aug 9 14:10:10 2015 +1000
    
        fuuuuu significant whitespace
    
    commit 1388967c84eabb3bfbea0d0dc738984736ab1525
    Author: Dave Cheney <dave@cheney.net>
    Date:   Sun Aug 9 14:07:41 2015 +1000
    
        fuuuuu significant whitespace
    
    commit 7737bda0f2d95b10675ce537e526159bc5752228
    Author: Dave Cheney <dave@cheney.net>
    Date:   Sun Aug 9 14:06:26 2015 +1000
    
        move source into correct location
    
    commit ebdb82273f1b839f5753733961395e188679edf6
    Author: Dave Cheney <dave@cheney.net>
    Date:   Sun Aug 9 14:02:07 2015 +1000
    
        more debugging
    
    commit 49e6d72a68ec5da3a0316e3dea016d8b5211cb9e
    Author: Dave Cheney <dave@cheney.net>
    Date:   Sun Aug 9 13:58:54 2015 +1000
    
        More fixes
    
    commit 96ee0f1f433963ab43d790a3e03974d037c13fb6
    Author: Dave Cheney <dave@cheney.net>
    Date:   Sun Aug 9 13:13:00 2015 +1000
    
        fix global flag parsing with plugins
    
    commit 98ebf859200a598d9dc2839e90821155e6c2768a
    Author: Dave Cheney <dave@cheney.net>
    Date:   Sun Aug 9 12:23:22 2015 +1000
    
        different git process
    
    commit 58621682d47c31716a7127602c8f4de41dff5ce2
    Author: Dave Cheney <dave@cheney.net>
    Date:   Sun Aug 9 12:20:30 2015 +1000
    
        Added sqlite3 integration test
    
    commit 592857f6ac057867eb7e7304180bafd118c5e02e
    Author: Dave Cheney <dave@cheney.net>
    Date:   Sun Aug 9 12:05:45 2015 +1000
    
        use go get rather than go build because wercker doesnt check out the code in the right place
    
    commit 78361fbe04c90446ade6232f4f29c5f61fb09e8a
    Author: Dave Cheney <dave@cheney.net>
    Date:   Sun Aug 9 11:57:36 2015 +1000
    
        skip the golint step for now
    
    commit a037b4a0423dc03f0425f9668645716802f6799b
    Author: Dave Cheney <dave@cheney.net>
    Date:   Sun Aug 9 11:56:12 2015 +1000
    
        box name is wercker/golang apparently
    
    commit 518fc4ecb5ea21a7b5f10b06ed43237603143180
    Author: Dave Cheney <dave@cheney.net>
    Date:   Sun Aug 9 11:53:53 2015 +1000
    
        first attempt

--- cmd/gb/main.go
+++ cmd/gb/main.go
@@ -50,7 +50,6 @@ func main() {
 			os.Exit(1)
 		}
 		command = commands["plugin"]
-		args = append([]string{"plugin"}, args...)
 	}
 
 	// add extra flags if necessary
@@ -68,7 +67,10 @@ func main() {
 		gb.Fatalf("could not parse flags: %v", err)
 	}
 
-	args = fs.Args()              // reset args to the leftovers from fs.Parse
+	args = fs.Args() // reset args to the leftovers from fs.Parse
+	if command == commands["plugin"] {
+		args = append([]string{name}, args...)
+	}
 	cwd, err := filepath.Abs(cwd) // if cwd was passed in via -R, make sure it is absolute
 	if err != nil {
 		gb.Fatalf("could not make project root absolute: %v", err)
