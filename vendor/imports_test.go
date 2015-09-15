611a3a16806ff7f76218fc456758d7c669469990
robert.hao <hypermind@outlook.com>
2015-09-15 12:37:15 +0800

    Improve hint & steps in the metadata fetch process.

--- vendor/imports_test.go
+++ vendor/imports_test.go
@@ -89,7 +89,7 @@ go get gopkg.in/check.v1
 		insecure: true,
 	}, {
 		path:     "any.inaccessible.server/the.project",
-		want:     `fail to access url "http://any.inaccessible.server/the.project?go-get=1"`,
+		want:     `fail to access url "https://any.inaccessible.server/the.project?go-get=1"`,
 		insecure: false,
 	}}
 
