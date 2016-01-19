087d40fa00c0fcfc94a480fa80b4f985f225ae06
Dave Cheney <dave@cheney.net>
2016-01-20 08:44:47 +1100

    Comment out bitbucket tests
    Bitbucket's inability to present a stable SSH host key causes CI to hang.

--- vendor/repo_test.go
+++ vendor/repo_test.go
@@ -43,18 +43,24 @@ func TestDeduceRemoteRepo(t *testing.T) {
 		want: &gitrepo{
 			url: "https://github.com/coreos/go-etcd",
 		},
-	}, {
-		path: "bitbucket.org/davecheney/gitrepo/cmd/main",
-		want: &gitrepo{
-			url: "https://bitbucket.org/davecheney/gitrepo",
-		},
-		extra: "/cmd/main",
-	}, {
-		path: "bitbucket.org/davecheney/hgrepo/cmd/main",
-		want: &hgrepo{
-			url: "https://bitbucket.org/davecheney/hgrepo",
-		},
-		extra: "/cmd/main",
+		/*
+			bitbucket cannot maintain a stable ssh key across their app servers
+			and this mucks up ci testing because mercurial does not have any
+			way of unconditionally accepting new ssh keys for the host.
+			Great work TEAM.
+			}, {
+				path: "bitbucket.org/davecheney/gitrepo/cmd/main",
+				want: &gitrepo{
+					url: "https://bitbucket.org/davecheney/gitrepo",
+				},
+				extra: "/cmd/main",
+			}, {
+				path: "bitbucket.org/davecheney/hgrepo/cmd/main",
+				want: &hgrepo{
+					url: "https://bitbucket.org/davecheney/hgrepo",
+				},
+				extra: "/cmd/main",
+		*/
 	}, {
 		path: "code.google.com/p/goauth2/oauth",
 		want: &hgrepo{
