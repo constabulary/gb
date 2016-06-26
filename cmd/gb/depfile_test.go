a6ff047d9c81d1719810b07e20378e355d5271b3
Dave Cheney <dave@cheney.net>
2016-06-26 14:24:57 +1000

    Added test for depfile dependant build (#622)
    * Added test for depfile dependant build
    
    * fix error spotted in windows build

--- cmd/gb/depfile_test.go
+++ cmd/gb/depfile_test.go
@@ -1,6 +1,10 @@
 package main_test
 
-import "testing"
+import (
+	"path/filepath"
+	"runtime"
+	"testing"
+)
 
 func TestMissingDepfile(t *testing.T) {
 	gb := T{T: t}
@@ -20,4 +24,39 @@ func main() {
 	gb.cd(gb.tempdir)
 	gb.runFail("build")
 	gb.grepStderr(`FATAL: command "build" failed:.+import "github.com/a/b": not found`, `import "github.com/a/b": not found`)
+}
+
+func TestDepfileFetchMissing(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test during -short")
+	}
+
+	gb := T{T: t}
+	defer gb.cleanup()
+
+	gb.tempDir("src/github.com/user/proj/a/")
+	gb.tempFile("src/github.com/user/proj/a/main.go", `package main
+
+import "github.com/pkg/profile"
+
+func main() {
+	defer profile.Start().Stop()
+}
+`)
+	gb.tempFile("depfile", `
+github.com/pkg/profile	version=1.1.0
+`)
+
+	gb.cd(gb.tempdir)
+	gbhome := gb.tempDir(".gb")
+	gb.setenv("GB_HOME", gbhome)
+	gb.setenv("DEBUG", ".")
+	gb.run("build")
+	name := "a"
+	if runtime.GOOS == "windows" {
+		name += ".exe"
+	}
+	gb.wantExecutable(gb.path("bin", name), "expected $PROJECT/bin/"+name)
+	gb.grepStdout("^fetching github.com/pkg/profile", "fetching pkg/profile not found")
+	gb.mustExist(filepath.Join(gbhome, "cache", "8fd41ea4fa48cd8435005bad56faeefdc57a25d6", "src", "github.com", "pkg", "profile", "profile.go"))
 }
