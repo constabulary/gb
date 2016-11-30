f592fc5f007912567bbd277c430f430a304de543
Dave Cheney <dave@cheney.net>
2016-11-30 19:08:25 +1100

    remove even more code

--- internal/importer/build.go
+++ internal/importer/build.go
@@ -5,11 +5,8 @@
 package importer
 
 import (
-	"bytes"
 	"fmt"
 	"go/build"
-	"path/filepath"
-	"strings"
 )
 
 // A Package describes the Go package found in a directory.
@@ -29,50 +26,4 @@ type MultiplePackageError struct {
 func (e *MultiplePackageError) Error() string {
 	// Error string limited to two entries for compatibility.
 	return fmt.Sprintf("found packages %s (%s) and %s (%s) in %s", e.Packages[0], e.Files[0], e.Packages[1], e.Files[1], e.Dir)
-}
-
-// expandSrcDir expands any occurrence of ${SRCDIR}, making sure
-// the result is safe for the shell.
-func expandSrcDir(str string, srcdir string) (string, bool) {
-	// "\" delimited paths cause safeCgoName to fail
-	// so convert native paths with a different delimeter
-	// to "/" before starting (eg: on windows).
-	srcdir = filepath.ToSlash(srcdir)
-
-	// Spaces are tolerated in ${SRCDIR}, but not anywhere else.
-	chunks := strings.Split(str, "${SRCDIR}")
-	if len(chunks) < 2 {
-		return str, safeCgoName(str, false)
-	}
-	ok := true
-	for _, chunk := range chunks {
-		ok = ok && (chunk == "" || safeCgoName(chunk, false))
-	}
-	ok = ok && (srcdir == "" || safeCgoName(srcdir, true))
-	res := strings.Join(chunks, srcdir)
-	return res, ok && res != ""
-}
-
-// NOTE: $ is not safe for the shell, but it is allowed here because of linker options like -Wl,$ORIGIN.
-// We never pass these arguments to a shell (just to programs we construct argv for), so this should be okay.
-// See golang.org/issue/6038.
-const safeString = "+-.,/0123456789=ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz:$"
-const safeSpaces = " "
-
-var safeBytes = []byte(safeSpaces + safeString)
-
-func safeCgoName(s string, spaces bool) bool {
-	if s == "" {
-		return false
-	}
-	safe := safeBytes
-	if !spaces {
-		safe = safe[len(safeSpaces):]
-	}
-	for i := 0; i < len(s); i++ {
-		if c := s[i]; c < 0x80 && bytes.IndexByte(safe, c) < 0 {
-			return false
-		}
-	}
-	return true
 }
